40fabd127165bd0775bfaa05f9810040bff55628
MarvinHongX <109493184+MarvinHongX@users.noreply.github.com>
2022-07-27 13:31:02 +0900

    Update 2_4_split_merge_linkedlist.py
    Correct typos

--- Python/2_4_split_merge_linkedlist.py
+++ Python/2_4_split_merge_linkedlist.py
@@ -4,7 +4,7 @@
 # Created : 8/11/2017
 # Modified: 8/11/2017
 # Notes   : [2.4] Write code to partition a linked list around a value x, such that all nodes less than x
-#                come before alt nodes greater than or equal to x.
+#                come before all nodes greater than or equal to x.
 #########################################################################################
 import unittest, os
 
