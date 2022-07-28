b4abeca60094acb087a035bfb81332ee633cd90f
MarvinHongX <109493184+MarvinHongX@users.noreply.github.com>
2022-07-28 14:20:33 +0900

    Update 2_5_sum_linkedlist.py
    Correct typos

--- Python/2_5_sum_linkedlist.py
+++ Python/2_5_sum_linkedlist.py
@@ -3,8 +3,8 @@
 # Author  : Hong
 # Created : 8/11/2017
 # Modified: 8/11/2017
-# Notes   : [2.6] You have two numbers represented by a linked list, where each node contains a
-#                single digit. The digits are stored in reverse order, such that the 1 's digit is at the head
+# Notes   : [2.5] You have two numbers represented by a linked list, where each node contains a
+#                single digit. The digits are stored in reverse order, such that the 1's digit is at the head
 #                of the list. Write a function that adds the two numbers and returns the sum as a linked list.
 #                FOLLOW UP
 #                Suppose the digits are stored in forward order. Repeat the above problem.
